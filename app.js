0d7658f454e56419a6a06406af258a33c24886ab
Vaughn2400 <kennethgolosinda24@gmail.com>
2021-01-27 17:23:10 +0800

    Homepage Changes

---
+++ app.js
@@ -0,0 +1,8 @@
+
+///////////////// NAV BAR SCROLL EFFECT///////////////
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('.navbar');
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('scrolling-active', windowPosition);
+})
+////////////////////////////////////////////////////
