51d3e89541f92d3ba62e84a16d7366194bf587ff
Vaughn2400 <kennethgolosinda24@gmail.com>
2021-01-28 13:33:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,24 +11,7 @@ window.addEventListener('scroll', function () {
 
 ///////////////// Add / Minus Button ///////////////
 function addItemToCart(food,amount) {
-    let btn_counter = true
-    if(document.querySelector("#cart-btn").textContent == "+"){
-        document.getElementById("cart-btn").style.padding = "5px 14px"
-        document.getElementById("cart-btn").style.backgroundColor = "#d6504b"
-        document.getElementById("cart-btn").innerHTML = "-";
-    }
-    else if(document.querySelector("#cart-btn").textContent == "-"){
-        document.getElementById("cart-btn").innerHTML = "+";
-        document.getElementById("cart-btn").style.padding = "5px 11px"
-        document.getElementById("cart-btn").style.backgroundColor = "#44c767"
 
-        
-    }
 
 }
 ////////////////////////////////////////////////////
-
-function addItemToCart(food,amount) {
-
-
-}
