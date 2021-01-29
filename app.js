c62f54499170421a827cf454681885c24bb4d984
Vaughn2400 <kennethgolosinda24@gmail.com>
2021-01-29 10:46:41 +0800

    Some Changes

--- app.js
+++ app.js
@@ -79,13 +79,15 @@ function quantityChanged(event) {
 }
 
 function addToCartClicked(event) { 
+    updateCartTotal()
     var button = event.target
     var shopItem = button.parentElement.parentElement
     var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
     var price = shopItem.getElementsByClassName('price')[0].innerText
     var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    
     addItemToCart(title, price, imageSrc)
-    updateCartTotal()
+
 }
 
 function addItemToCart(title, price, imageSrc) {
@@ -101,7 +103,7 @@ function addItemToCart(title, price, imageSrc) {
     }
     var cartRowContents = `
         <div class="cart-item cart-column">
-            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <img class="cart-item-image" src="${imageSrc}" width="50%" height="50%">
             <span class="cart-item-title">${title}</span>
         </div>
         <span class="cart-price cart-column">${price}</span>
@@ -113,20 +115,27 @@ function addItemToCart(title, price, imageSrc) {
     cartItems.append(cartRow)
     cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
     cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+    
+
+
 }
 
 function updateCartTotal() {
+
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
     var total = 0
+
     for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('price')[0]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
         var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
         var price = parseFloat(priceElement.innerText.replace('₱', ''))
         var quantity = quantityElement.value
         total = total + (price * quantity)
     }
     total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = '₱' + total
+
+
+    document.getElementsByClassName('cart-total-price')[0].innerText = "₱"+ total
 }
